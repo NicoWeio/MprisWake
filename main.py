68fd784058f4b741ead39347d77752584fca98c2
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-12-07 01:04:45 +0100

    adapted main.py to use my wake alternative

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from time import sleep
 
 import dbus
+
 # import wakepy
+import mywake
 
 bus = dbus.SessionBus()
 spotify_obj = bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
 spotify_pm = dbus.Interface(spotify_obj, 'org.freedesktop.DBus.Properties')
 
+
+def is_playing():
+    return spotify_pm.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus') == 'Playing'
+
+
 while True:
-    playback_status = spotify_pm.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
-    match playback_status:
-        case 'Playing':
-            wakepy.set_keepawake()
-        case 'Paused' | 'Stopped':
-            wakepy.unset_keepawake()
-    print(playback_status)
+    if is_playing():
+        with mywake.inhibit('Spotify', 'Playing music'):
+            print("Inhibited")
+            while is_playing():
+                sleep(5)
+            print("Uninhibited")
     sleep(5)
